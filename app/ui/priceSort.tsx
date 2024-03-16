fd7ebbdd26c5f286d841d0d8c3b3b01d2ad4f6ea
vasileloh <117695183+vasileloh@users.noreply.github.com>
2024-03-16 17:55:07 +0200

    added pagination

--- app/ui/priceSort.tsx
+++ app/ui/priceSort.tsx
@@ -62,7 +62,7 @@ export const  PriceSort =  () => {
            className="bg-white peer h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-400 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 empty:!bg-gray-900 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
            >
              {options.map((option) => (
-              <option value={option.label}>{option.label}</option>
+              <option key={option.label} value={option.label}>{option.label}</option>
              )
 
              )}
