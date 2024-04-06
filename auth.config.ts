1c4f35a4393d9ad185a121606dbca782c57501a9
vasileloh <117695183+vasileloh@users.noreply.github.com>
2024-04-06 17:46:12 +0300

    admin config

--- auth.config.ts
+++ auth.config.ts
@@ -3,16 +3,17 @@ import type { NextAuthConfig } from 'next-auth';
 export const authConfig = {
   pages: {
     signIn: '/login',
+    signOut: '/#'
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/admin');
-      if (isOnDashboard) {
+      const isOnAdmin = nextUrl.pathname.startsWith('/admin');
+      if (isOnAdmin) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        return Response.redirect(new URL('/admin', nextUrl));
+        return Response.redirect(new URL('/#', nextUrl));
       }
       return true;
     },
